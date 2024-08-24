90ded128368270595d10e205c54aae26f97b23ad
David Rolf <leepicrockyc@gmail.com>
2024-08-24 07:00:27 -0400

    Added proper movement to the player lmao

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,6 +4,13 @@ extends CharacterBody3D
 
 @export var spring_arm : SpringArm3D
 
+enum {
+	UNHANDLED,
+	IDLE,
+	MOVE,
+	JUMP
+}
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -14,25 +21,17 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		
+	move(delta)
 
 func move(delta : float):
 	var input_vector : Vector3 = Vector3(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 	0,
-	Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
+	Input.get_action_strength("move_down") - Input.get_action_strength("move_up")).normalized()
+	
+	input_vector = input_vector.rotated(Vector3.UP, spring_arm.rotation.y)
+	
+	velocity.x = input_vector.x * SPEED
+	velocity.z = input_vector.z * SPEED
+	
+	move_and_slide()
